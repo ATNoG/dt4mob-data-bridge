4c5cb94168a8c11ad203112cab84a5d8a37ba3f0
iavcoelho <118473375+iavcoelho@users.noreply.github.com>
2026-06-20 00:44:55 +0100

    feat: implement the strategy for acquiring meteorologic data

---
+++ strategies/meteo.py
@@ -0,0 +1,28 @@
+from typing import List, final
+
+from loguru import logger
+
+from interfaces.ipma import get_meteorology_measurements
+from models.ditto import DittoProtocolEnvelope, Feature
+from models.meteo import Measurement, Station
+from strategies.strategy import BaseStrategy
+
+
+@final
+class MeteoStrategy(BaseStrategy):
+    async def get_telemetry(self) -> List[DittoProtocolEnvelope]:
+        logger.info("Updating metereology stations' information")
+        measurements = await get_meteorology_measurements()
+        logger.debug("Got measurements from IPMA")
+
+        return [self.envelope_from_measurement(tup) for tup in measurements]
+
+    def envelope_from_measurement(
+        self,
+        tup: tuple[Station, Measurement],
+    ) -> DittoProtocolEnvelope:
+        station, measurement = tup
+        attributes = station.model_dump()
+        features = {"meteorology": Feature(properties=measurement.model_dump())}
+        topic = self.create_topic(str(station.id))
+        return self.create_envelope(topic, attributes, features)
